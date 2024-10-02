cb6b055b6c69cff60554e09c3f5b35562886fda2
Seeongh <mikstic7@gmail.com>
2024-10-02 23:56:02 +0900

    <study> java.lang - 자바의 기본 패키지

---
+++ src/lang/LangMain.java
@@ -0,0 +1,17 @@
+package lang;
+
+/**
+ * java lang
+ * : 자동 import로 생략 가능
+ * import java.lang
+ */
+public class LangMain {
+    public static void main(String[] args) {
+        /**
+         * 대표 class
+         * System, String
+         */
+
+        System.out.println("java.lang");
+    }
+}
