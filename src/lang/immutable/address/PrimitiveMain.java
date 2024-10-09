907a21cd4952b0ad686fb79ddcfef9595d754aaa
Seeongh <mikstic7@gmail.com>
2024-10-09 10:07:20 +0900

    <study> 불변 객체 - 기본형과 참조형

---
+++ src/lang/immutable/address/PrimitiveMain.java
@@ -0,0 +1,20 @@
+package lang.immutable.address;
+
+/**
+ * 불변객체
+ */
+public class PrimitiveMain {
+    public static void main(String[] args) {
+        //기본형은 절대로 같은 값을 공유하지 않는다.
+        int a = 10;
+        int b = a;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        b=20;
+        System.out.println("b = 20");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
