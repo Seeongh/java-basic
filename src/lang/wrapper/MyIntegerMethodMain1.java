b5807f881a6ffcd75539b2c689bbcd48992ea321
Seeongh <mikstic7@gmail.com>
2024-10-15 23:55:30 +0900

    <study> Wrapper class - 기본형의 한계로 외부 메서드 사용

---
+++ src/lang/wrapper/MyIntegerMethodMain1.java
@@ -0,0 +1,18 @@
+package lang.wrapper;
+
+/**
+ * 기본형의 한계로 참조로 변경
+ */
+public class MyIntegerMethodMain1 {
+    public static void main(String[] args) {
+        MyInteger myInteger = new MyInteger(10);
+        myInteger.compareTo(5);
+        myInteger.toString();
+
+        myInteger.compareTo(10);
+        myInteger.compareTo(20);
+
+
+
+    }
+}
