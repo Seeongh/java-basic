6f1195802d2150ea7d1ce5460b2685ce0fe5f0a4
Seeongh <mikstic7@gmail.com>
2024-11-03 23:59:03 +0900

    <study> 익명 클래스 활용 - 효율성의 핵심은 변하는 부분을 분리하여 재사용함

---
+++ src/anonymous/ex0/Ex0Maijn.java
@@ -0,0 +1,15 @@
+package anonymous.ex0;
+
+public class Ex0Maijn {
+    public static void main(String[] args) {
+        System.out.println("시작");
+        System.out.println("hello");
+        System.out.println("종료");
+    }
+
+    public static void helloSpring() {
+        System.out.println("시작");
+        System.out.println("hello spring");
+        System.out.println("종료");
+    }
+}
---
+++ src/anonymous/ex0/Ex0RefMain.java
@@ -0,0 +1,18 @@
+package anonymous.ex0;
+
+/**
+ * 중복 제거와 효율성의 핵심은 변하는 부분을 분리하여 재사용
+ */
+public class Ex0RefMain {
+    public static void hello(String helloStr) {
+        System.out.println("시작");
+        System.out.println(helloStr);
+        System.out.println("종료");
+    }
+
+    public static void main(String[] args) {
+        hello("java");
+        hello("spring");
+
+    }
+}
