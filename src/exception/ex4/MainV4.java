081da23300cc056b4475cdbaf2141dcffcae7d2e
Seeongh <mikstic7@gmail.com>
2024-11-24 23:29:19 +0900

    <study> Exception : instanceOf를 활용한 공통 로직에서 별도 예외처리

--- src/exception/ex4/MainV4.java
+++ src/exception/ex4/MainV4.java
@@ -1,6 +1,8 @@
 package exception.ex4;
 
 
+import exception.ex4.exception.SendExceptionV4;
+
 import java.util.Scanner;
 
 public class MainV4 {
@@ -33,6 +35,12 @@ public class MainV4 {
     //공통 예외 로직
     private static void exceptionHandler(Exception e) {
         System.out.println("사용자 메세지 : " +  e.getMessage());
-        e.printStackTrace();
+        //e.printStackTrace();
+        e.printStackTrace(System.out); // System.error : 흐름이 다름 
+
+        //별도 예외추가
+        if( e instanceof SendExceptionV4 sendEx) {
+            System.out.println("전송 데이터 : " + sendEx);
+        }
     }
 }
