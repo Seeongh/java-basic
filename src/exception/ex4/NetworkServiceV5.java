757d78f3b4f4014f49f53cc0aaa65676f28341eb
Seeongh <mikstic7@gmail.com>
2024-11-26 09:21:14 +0900

    <study> Exception - Autocloseable을 구현한 class, try후 close 호출

--- src/exception/ex4/NetworkServiceV5.java
+++ src/exception/ex4/NetworkServiceV5.java
@@ -1,19 +1,19 @@
 package exception.ex4;
 
-import exception.ex4.NetworkClientV5;
 public class NetworkServiceV5 {
 
-    public void sendMessage(String data){
+    public void sendMessage(String data) {
         String address="http://example.com";
 
-        try(NetworkClientV5 client = new NetworkClientV5((address)) {
+        try(NetworkClientV5 client = new NetworkClientV5(address)) { //자원을 명시적으로 지정. try문이 끝나면 해당 자원의 close 자동 호출
             client.initError(data);
             client.connect();
-             client.send(data);
-        } catch( Exception e) {
+            client.send(data);
+
+        }
+        catch(Exception e) {
             System.out.println("예외 : " + e.getMessage());
             throw e;
         }
-
     }
 }
