9d9639f6ba16288fb4976bb4f9956c57b59e3f5b
Seeongh <mikstic7@gmail.com>
2024-11-29 11:12:14 +0900

    <study>Generic : Object를 통한 타입 통일로 중복 제거, 재사용성 높혔으나 타입 안정성 떨어짐

--- src/generic/ex1/BoxMain2.java
+++ src/generic/ex1/BoxMain2.java
@@ -1,2 +1,29 @@
-package generic.ex1;public class BoxMain2 {
+package generic.ex1;
+
+/**
+ * Object를 사용함으로 중복에러는 개선했으나 문제가 있음
+ * 1. 반환 타입이 맞지 않음 *다운캐스팅
+ * 2. 잘못된 타입의 인수 전달 문제 (타입안정성이 낮음)
+ */
+public class BoxMain2 {
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10) ;
+        Object obj = integerBox.get();
+        Integer integer = (Integer) obj;
+        System.out.println("integer  = " + integer); //Object => Integer 캐스팅
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("hello");
+        String str = (String) stringBox.get(); // Object => String 캐스팅
+
+        System.out.println("string = " + str);
+
+        // 잘못된 타입의 인수 전달
+        integerBox.set("문자100");
+        Integer result = (Integer) integerBox.get();  // String -> Integer  캐스팅 예외
+        System.out.println("error = " + result);
+
+
+    }
 }
--- src/generic/ex1/ObjectBox.java
+++ src/generic/ex1/ObjectBox.java
@@ -1,2 +1,17 @@
-package generic.ex1;public class ObjectBox {
+package generic.ex1;
+
+/**
+ * 모든 타입을 담을 수 있는 BOX
+ */
+public class ObjectBox {
+
+    private Object value;
+
+    public void set(Object obj) {
+        this.value = obj;
+    }
+
+    public Object get() {
+        return this.value;
+    }
 }
