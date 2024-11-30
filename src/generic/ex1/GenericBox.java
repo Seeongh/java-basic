945f8269a76c6339d5353a733731c87fca98af66
Seeongh <mikstic7@gmail.com>
2024-11-30 11:12:42 +0900

    <study>Generic :  T타입을 매개변수로 받는 Generic class 생성

--- src/generic/ex1/GenericBox.java
+++ src/generic/ex1/GenericBox.java
@@ -1,4 +1,19 @@
 package generic.ex1;
 
-public class ㅎ둗걏ㅠㅐㅌ {
+/**
+ * 제네릭 클래스
+ * 1. 타입을 미리 결정하지 않는다.
+ * 2. <T>타입이 나중에 결정됨 T :타입 매개변수
+ * @param <T>
+ */
+public class GenericBox<T> {
+    private T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
 }
