1360a7df3a59c0ed42db46f9543fa41e4ec5e7e2
Seeongh <mikstic7@gmail.com>
2024-12-17 23:14:59 +0900

    <study>Generic - Wildcard : 하한 제한, 상한제한

--- src/generic/ex5/WildcardMain1.java
+++ src/generic/ex5/WildcardMain1.java
@@ -16,5 +16,15 @@ public class WildcardMain1 {
 
         WildCardEx.<Dog>printWildcardV1(dog);
         WildCardEx.<Dog>printWildcardV2(dog);
+
+        //cat을 넣으면 그대로 cat이 나오게
+        //제네릭의 경우 문제가 안됨.
+        cat.set(new Cat("냐옹스", 200));
+        Cat catBox = WildCardEx.printAndReturnGeneric(cat);
+
+        //Wildcard를 쓰는 printAndReturnWildcard의 경우 받아는 져도 Animal 로 들어가기 때문에 cat으로 꺼낼수가 없음
+        //즉 받을떄만 걍 암거나 받는다는거지, 타입이 다 바뀌는게 아님. (캐스팅 필요)
+
+
     }
 }
