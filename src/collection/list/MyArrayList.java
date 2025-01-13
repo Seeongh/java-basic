27bd1e62b8c893e055a585291481a494f6c8b214
Seeongh <mikstic7@gmail.com>
2025-01-13 17:14:58 +0900

    <study> 컬렉션 프레임 워크 - 성능비교(중간 삽입 및 검색)

--- src/collection/list/MyArrayList.java
+++ src/collection/list/MyArrayList.java
@@ -16,7 +16,7 @@ public class MyArrayList<E> implements MyList<E>{
     }
 
     public MyArrayList(int initialCapacity) {
-        elementData = new Object[initialCapacity]; //제네릭은 런타임에 이레이저에 의해 타입정보가 다 사라져 new E[]는 불가능
+        elementData = new Object[initialCapacity];
     }
 
     @Override
@@ -26,91 +26,79 @@ public class MyArrayList<E> implements MyList<E>{
 
     @Override
     public void add(E e) {
-        //코드 추가
-        if( size == elementData.length) {
+        if (size == elementData.length) {
             grow();
         }
-        elementData[size] = 0;
+        elementData[size] = e;
         size++;
     }
 
     @Override
     public void add(int index, E e) {
-        //코드 추가
-        if( size == elementData.length) {
+        if (size == elementData.length) {
             grow();
         }
-
-        //데이터 이동
-        shiftRightFrom(index); //오른쪽으로 이동
-        elementData[size] = 0;
+        shiftRightFrom(index);
+        elementData[index] = e;
         size++;
     }
 
-    //dythdml akwlakrqnxj ㅑㅜㅇㄷㅌRKwl dhfmsWHrdmfh alfrl
+    //요소의 마지막부터 index까지 오른쪽으로 밀기
     private void shiftRightFrom(int index) {
-        for (int i = size; i > index ; i--) {
-            elementData[i] = elementData[i-1];
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
         }
-
     }
 
     @Override
-    public E remove(int index) {
-        E oldValue = get(index) ;
-        shiftLeftFrom(index) ; //인덱스부터 왼쪽으로 민다.
-        size--;
-        elementData[size] = null;
-        return oldValue;
-    }
-
-    private void shiftLeftFrom(int index) {
-        for(int i = index; i<size-1; i++) {
-            elementData[i] = elementData[i+1];
-        }
-    }
-
-    //코드 추가
-    private void grow() {
-        int oldCapacity = elementData.length;
-        int newCapacity = oldCapacity * 2;
-        //배열을 새로 만들고, 기존 배열을 새로운 배열에 복사
-//        Object[] newArr = new Object[newCapacity];
-
-//        for(int i =0 ; i< elementData.length; i++) {
-//            newArr[i]  = elementData[i];
-//        }
-
-        Object[] newArr = Arrays.copyOf(elementData, newCapacity);
-        elementData = newArr; //가리키는 참조값을 바꿈.
-    }
-
     @SuppressWarnings("unchecked")
-    @Override
     public E get(int index) {
-        return (E)elementData[index];
-    } //다운캐스팅을 해주어야함 (들어간게 e타입이기 때문에)
-
+        return (E) elementData[index];
+    }
 
     @Override
     public E set(int index, E element) {
         E oldValue = get(index);
         elementData[index] = element;
         return oldValue;
-
     }
 
     @Override
-    public int indexOf(Object o) {
-        for(int i =0 ; i< size; i++) {
-            if(o.equals(elementData[i])) {
+    public E remove(int index) {
+        E oldValue = get(index);
+        shiftLeftFrom(index);
+
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    //요소의 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+
+    @Override
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
                 return i;
             }
         }
         return -1;
     }
 
-    public String toString() {
-        return Arrays.toString(Arrays.copyOf(elementData, size)) + "size = " + size + " , capacity = " + elementData.length;
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
     }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) + " size=" + size + ", capacity=" + elementData.length;
+    }
+
 }
--- src/collection/list/MyListPerfomanceTest.java
+++ src/collection/list/MyListPerfomanceTest.java
@@ -5,12 +5,16 @@ package collection.list;
  */
 public class MyListPerfomanceTest {
     public static void main(String[] args) {
-        int size = 50_000;
+        //int size = 1000;
         System.out.println("myarrayList 추가 ==");
-        addFirst(new MyArrayList<>(), size);
+       // addFirst(new MyArrayList<>(), size);
+       // addMid(new MyArrayList<>(), size);
+        //addLast(new MyArrayList<>(), size);
 
         System.out.println("mylinkedlist추가 ==");
-        addFirst(new MyLinkedList<>(), size);
+       // addFirst(new MyLinkedList<>(), size);
+      //  addMid(new MyLinkedList<>(), size);
+       // addLast(new MyLinkedList<>(), size);
 
     }
 
@@ -22,4 +26,23 @@ public class MyListPerfomanceTest {
         long endTime = System.currentTimeMillis();
         System.out.println("앞에 추가 - 크기 : " + size + " , 계산 시간 " + (endTime-startTime) + "ms");
     }
+
+    private static void addMid(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i< size; i++) {
+            list.add(i/2,i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("중간에 추가 - 크기 : " + size + " , 계산 시간 " + (endTime-startTime) + "ms");
+    }
+
+    private static void addLast(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i< size; i++) {
+            list.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("뒤에 추가 - 크기 : " + size + " , 계산 시간 " + (endTime-startTime) + "ms");
+    }
+
 }
