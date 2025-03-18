52428267d040c90e39b36f6567cc8f0db983d40b
Seeongh <mikstic7@gmail.com>
2025-03-18 23:03:38 +0900

    <study> collection util - max, min, shuffle

---
+++ src/collection/utils/CollectionSortMain.java
@@ -0,0 +1,25 @@
+package collection.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionSortMain {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        Integer max = Collections.max(list);
+        System.out.println("max = " + max);
+
+        Integer min = Collections.min(list);
+        System.out.println("min = " + min);
+
+        Collections.shuffle(list);
+        System.out.println("suffle list = " + list);
+    }
+}
