efb9fbb1c7c48a02429dc78831ff16b8195479a7
Seeongh <mikstic7@gmail.com>
2025-04-02 23:39:03 +0900

    <study> 빈 list 만들기 (가변/불변)

---
+++ src/collection/utils/EmptyListMain.java
@@ -0,0 +1,22 @@
+package collection.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 빈 리스트
+ */
+public class EmptyListMain {
+    public static void main(String[] args) {
+        //빈 가변 리스트 생성
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+        //빈 불변 리스트 생성
+        List<Object> list3 = Collections.emptyList();
+        List<Integer> objects = List.of(); 
+
+    }
+}
