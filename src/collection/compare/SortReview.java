29504bbf58721f78527325becd3b8237e14b63b2
Seeongh <mikstic7@gmail.com>
2025-04-18 23:42:47 +0900

    <review> 비교자 생성 복습

---
+++ src/collection/compare/SortReview.java
@@ -0,0 +1,24 @@
+package collection.compare;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortReview {
+    public static void main(String[] args) {
+        Integer[] array = {3,2,1};
+        System.out.println(Arrays.toString(array));
+
+        //기본정렬
+        Arrays.sort(array); //1,2,3
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    static class AscOmparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1.compareTo(o2); //o1이 작으면 음수 같으면 0 , 크면 양수
+        }
+    }
+}
